256722da79537092cfda62784a88b12d98bfc38d
gonzaloiv <aelthalion@gmail.com>
2017-04-06 20:17:41 +0900

    Funcionamiento básico de la nave y efecto wrapping

---
+++ Assets/Scripts/GameObjects/Common/WrappingBehaviour.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Renderer))]
+public class WrappingBehaviour : MonoBehaviour {
+
+  #region Fields
+
+  private Vector2 screenSize;
+
+  #endregion
+
+  #region Mono Behaviour
+
+  void Awake() {
+    screenSize = Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, Screen.height));
+  }
+
+  void Update() {
+    if (transform.position.x > screenSize.x)
+      transform.position = new Vector2(-screenSize.x, transform.position.y);
+    if (transform.position.x < -screenSize.x)
+      transform.position = new Vector2(screenSize.x, transform.position.y);
+    if (transform.position.y > screenSize.y)
+      transform.position = new Vector2(transform.position.x, -screenSize.y);
+    if (transform.position.y < -screenSize.y)
+      transform.position = new Vector2(transform.position.x, screenSize.y);
+  }
+
+  #endregion
+
+}
---
+++ Assets/Scripts/GameObjects/Player/PlayerController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider2D))]
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerController : MonoBehaviour {
+
+  #region Fields Behaviour
+
+  [SerializeField] private float thrust = 0.0001f;
+  [SerializeField] private float rotationSpeed = 0.00001f;
+  private Rigidbody2D rb;
+
+  #endregion
+
+  #region Mono Behaviour
+
+  void Awake() {
+    rb = GetComponent<Rigidbody2D>();
+  }
+
+  void OnEnable() {
+    EventManager.StartListening<MoveUpInput>(OnMoveUpInput);
+    EventManager.StartListening<MoveRightInput>(OnMoveRightInput);
+    EventManager.StartListening<MoveLeftInput>(OnMoveLeftInput);
+  }
+
+  void OnDisable() {
+    EventManager.StopListening<MoveUpInput>(OnMoveUpInput);
+    EventManager.StopListening<MoveRightInput>(OnMoveRightInput);
+    EventManager.StopListening<MoveLeftInput>(OnMoveLeftInput);
+  }
+
+  #endregion
+
+  #region Event Behaviour
+
+  void OnMoveUpInput(MoveUpInput moveUpInput) {
+    rb.AddForce(transform.up * thrust);
+  }
+
+  void OnMoveRightInput(MoveRightInput moveRightInput) {
+    transform.Rotate(-Vector3.forward * rotationSpeed * Time.timeScale);
+  }
+
+  void OnMoveLeftInput(MoveLeftInput moveLeftInput) {
+    transform.Rotate(Vector3.forward * rotationSpeed * Time.timeScale);
+  }
+
+  #endregion
+	
+}
