93f17f25ae569a65ee1cbdbfaabbc26659131233
gonzaloiv <aelthalion@gmail.com>
2017-04-06 14:50:46 +0900

    Setup del proyecto en Unity y del repositorio en Github

---
+++ Assets/Scripts/Events.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine.Events;
+using UnityEngine;
+
+#region Player Input Events
+
+public class MoveRightInput : UnityEvent {}
+public class MoveLeftInput : UnityEvent {}
+public class MoveUpInput : UnityEvent {}
+public class SpaceInput : UnityEvent {}
+public class EscapeInput : UnityEvent {}
+
+#endregion
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+  #region Mono Behaviour
+
+  void OnEnable() {
+    EventManager.StartListening<SpaceInput>(OnSpaceInput);
+  }
+
+  void OnDisable() {
+    EventManager.StopListening<SpaceInput>(OnSpaceInput);
+  }
+
+  #endregion 
+
+  #region Event Behaviour
+
+  void OnSpaceInput(SpaceInput spaceInput) {
+    Debug.Log("Game awakening!");
+  }
+
+  #endregion
+
+}
